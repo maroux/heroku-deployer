e807055ad762f595878d3eff576250ceecfe44ec
Aniruddha Maru <aniruddha@automatic.com>
2013-12-16 14:05:32 -0600

    ++ Changes

--- lib/heroku_deployer.rb
+++ lib/heroku_deployer.rb
@@ -3,15 +3,17 @@ require 'git'
 require 'zlib'
 require 'ostruct'
 require 'logger'
+require 'date'
+require 'optparse'
 
 class HerokuDeployer
   attr_reader :app, :logger, :post_payload
 
   def self.exists?(app)
-    !!(ENV["#{app}_HEROKU_REPO"] && ENV["#{app}_GIT_REPO"] && ENV["#{app}_SSH_KEY"])
+    !!(ENV["#{app}_HEROKU_STAGE_REPO"] && ENV["#{app}_HEROKU_MASTER_REPO"] && ENV["#{app}_GIT_REPO"] && ENV["#{app}_SSH_KEY"])
   end
 
-  def initialize(app_name, post_payload, logger = Logger.new(STDOUT))
+  def initialize(app_name, post_payload = nil, logger = Logger.new(STDOUT))
     @app = app_name
     @post_payload = post_payload
     @logger = logger
@@ -20,9 +22,11 @@ class HerokuDeployer
   def deploy
     tries = 0
     begin
-      if post_payload['ref'] == "refs/heads/#{config.git_branch}"
-          update_local_repository
-          push
+      # only merge next to staging during non-code freeze time 
+      if post_payload['ref'] == "refs/heads/#{config.git_next_branch}" and
+          (DateTime.now().new_offset('-0800').hour < 14 or
+          DateTime.now().new_offset('-0800').hour >= 23)
+              update_staging_env
       end
     rescue
       tries += 1
@@ -36,12 +40,22 @@ class HerokuDeployer
 
   private
 
+  def update_staging_env
+      update_local_repository(config.git_next_branch)
+      update_local_repository(config.git_staging_branch)
+      merge
+      push
+  end
+
   def config
     @config ||= OpenStruct.new({
-      heroku_repo: ENV["#{app}_HEROKU_REPO"],
+      heroku_stage_repo: ENV["#{app}_HEROKU_STAGE_REPO"],
+      heroku_master_repo: ENV["#{app}_HEROKU_MASTER_REPO"],
       git_repo: ENV["#{app}_GIT_REPO"],
       ssh_key: ENV["#{app}_SSH_KEY"],
-      git_branch: ENV["#{app}_GIT_BRANCH"] || "master",
+      git_next_branch: ENV["#{app}_git_next_branch"] || "next",
+      git_staging_branch: ENV["#{app}_git_staging_branch"] || "staging",
+      git_master_branch: ENV["#{app}_git_master_branch"] || "master",
     })
   end
 
@@ -53,26 +67,112 @@ class HerokuDeployer
     Dir.exists?(File.join(local_folder, '.git'))
   end
 
-  def update_local_repository
+  def update_local_repository(branch)
     GitSSHWrapper.with_wrapper(:private_key => config.ssh_key) do |wrapper|
       wrapper.set_env
       clone unless repo_exists?
       logger.info "fetching"
-      logger.debug `cd #{local_folder} && git fetch && git reset --hard origin/#{config.git_branch}`
+      logger.debug `cd #{local_folder} && git checkout #{branch} && git fetch && git reset --hard origin/#{branch}`
     end
+  end
+
+  def merge
+      GitSSHWrapper.with_wrapper(:private_key => config.ssh_key) do |wrapper|
+          wrapper.set_env
+          logger.debug `cd #{local_folder} && git merge #{config.git_next_branch} -m "Auto-merging #{config.git_next_branch} to #{config.git_staging_branch}"` 
+      end
   end
 
   def clone
     logger.info "cloning"
-    logger.debug `git clone -b #{config.git_branch} #{config.git_repo} #{local_folder}`
-    logger.debug `cd #{local_folder} && git remote add heroku #{config.heroku_repo}`
+    logger.debug `git clone #{config.git_repo} #{local_folder}`
+    logger.debug `cd #{local_folder} && git remote add heroku-stage #{config.heroku_stage_repo} && git remote add heroku-master #{config.heroku_master_repo}`
   end
 
   def push
+    GitSSHWrapper.with_wrapper(:private_key => config.ssh_key) do |wrapper|
+      wrapper.set_env
+      logger.info "pushing to github"
+      logger.debug `cd #{local_folder}; git push origin #{config.git_staging_branch}`
+    end
     GitSSHWrapper.with_wrapper(:private_key => ENV['DEPLOY_SSH_KEY']) do |wrapper|
       wrapper.set_env
-      logger.info "pushing"
-      logger.debug `cd #{local_folder}; git push -f heroku #{config.git_branch}:master`
+      logger.info "pushing to heroku"
+      logger.debug `cd #{local_folder}; git push -f heroku-stage #{config.git_staging_branch}:master`
     end
   end
+
+  def get_commit_to_merge
+      GitSSHWrapper.with_wrapper(:private_key => config.ssh_key) do |wrapper|
+          wrapper.set_env
+          clone unless repo_exists?
+          logger.info 'Fetching commit to merge'
+          # update both branches
+          update_local_repository(config.git_next_branch)
+          update_local_repository(config.git_staging_branch)
+          before_date = DateTime.new(year=DateTime.now.year, 
+                                     month=DateTime.now.month, 
+                                     day=DateTime.now.day, 
+                                     hour=14, 
+                                     minute=0, 
+                                     second=0, 
+                                     '-8')
+          after_date = DateTime.rfc2822(`cd #{local_folder} && git log -n1 --pretty="%cD" #{config.git_staging_branch}`).new_offset("-0800")
+          before_date_str = before_date.strftime('%m-%d-%Y %H:%M:%S %z') 
+          after_date_str = after_date.strftime('%m-%d-%Y %H:%M:%S %z')
+          logger.info "Looking for commit after #{after_date_str} and before #{before_date_str}"
+          commit_hash = `cd #{local_folder} && git log -n1 --after="#{after_date_str}" --before="#{before_date_str}" --pretty="%H #{config.git_next_branch}"`
+          return commit_hash
+      end
+  end
+
+  public 
+
+  def merge_to_master(dry_run='')
+      update_local_repository(config.git_staging_branch)
+      update_local_repository(config.git_master_branch)
+      GitSSHWrapper.with_wrapper(:private_key => config.ssh_key) do |wrapper|
+        wrapper.set_env
+        logger.debug `cd #{local_folder} && git merge #{config.git_staging_branch} -m "Auto-merging #{config.git_staging_branch} to #{config.git_master_branch}"`
+        logger.debug `cd #{local_folder} && git push #{dry_run} origin #{config.git_master_branch}`
+      end
+      GitSSHWrapper.with_wrapper(:private_key => ENV['DEPLOY_SSH_KEY']) do |wrapper|
+        wrapper.set_env
+        logger.debug `cd #{local_folder} && git push #{dry_run} -f heroku_master #{config.git_master_branch}:master`
+      end
+      update_staging_env 
+  end 
+end
+
+if __FILE__ == $0
+    options = {}
+    opt_parser = OptionParser.new do |opt|
+        opt.banner = "Usage: opt_parser COMMAND [OPTIONS]"
+        opt.separator  ""
+        opt.separator  "Commands"
+        opt.separator  "     merge_to_master"
+        opt.separator  ""
+        opt.separator  "Options"
+
+        opt.on("-d","--dry-run","dry run - will not execute push") do
+            options[:dry_run] = "--dry-run" 
+        end
+
+        opt.on("-h","--help","help") do
+            puts opt_parser
+        end
+    end
+
+    opt_parser.parse!
+
+    case ARGV[0]
+    when "merge_to_master"
+        if not HerokuDeployer.exists?("server_heroku")
+            print "no app found\n"
+            exit 0 
+        end
+        HerokuDeployer.new("server_heroku").merge_to_master(options[:dry_run] || '')
+    else
+        puts opt_parser
+    end
 end
