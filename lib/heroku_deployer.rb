f384fa520860b8f7fbec5fc84af9b2422153429b
Aniruddha Maru <aniruddha@automatic.com>
2014-01-15 19:07:14 +0530

    ++ Fixes issue - time lock not working

--- lib/heroku_deployer.rb
+++ lib/heroku_deployer.rb
@@ -24,7 +24,7 @@ class HerokuDeployer
     begin
       # only merge next to staging during non-code freeze time 
       if post_payload['ref'] == "refs/heads/#{config.git_next_branch}" and
-          (DateTime.now().new_offset('-0800').hour < 14 or
+          (DateTime.now().new_offset('-0800').hour < 14 and 
           DateTime.now().new_offset('-0800').hour >= 0)
               update_staging_env
       end
@@ -44,7 +44,9 @@ class HerokuDeployer
       update_local_repository(config.git_next_branch)
       update_local_repository(config.git_staging_branch)
       merge
+      maint_on
       push
+      maint_off
   end
 
   def config
@@ -66,6 +68,9 @@ class HerokuDeployer
   def repo_exists?
     Dir.exists?(File.join(local_folder, '.git'))
   end
+
+  def maint_on
+
 
   def update_local_repository(branch)
     GitSSHWrapper.with_wrapper(:private_key => config.ssh_key) do |wrapper|
